d0da1279eb274980e9e63965d4ff6ed1ab21a7f6
Sebastian Krzyszkowiak <dos@dosowisko.net>
2015-01-31 18:31:30 +0100

    better check for losing moves

--- src/put/ai/games/ourplayer/OurPlayer.java
+++ src/put/ai/games/ourplayer/OurPlayer.java
@@ -1,14 +1,11 @@
 package put.ai.games.ourplayer;
 
 import java.util.List;
-import java.util.Random;
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
 public class OurPlayer extends Player {
-
-    private final Random random = new Random(0xdeadbeef);
 
     @Override
     public String getName() {
@@ -37,35 +34,43 @@ public class OurPlayer extends Player {
         		tieingMove = move;
         	}
         	if (winner == null) {
-        		nothingSpecialMove = move;
+        		
+        		Board btemp2 = btemp.clone();
+        		
+        		List<Move> opponentMoves = btemp2.getMovesFor(getOpponent(getColor()));
+        		
+        		for (Move opponentMove: opponentMoves) {
+        			btemp2.doMove(opponentMove);
+            		Color winner2 = btemp2.getWinner(getColor());
+            		
+            		if (winner2 == getOpponent(getColor())) {
+            			losingMove = move;
+            			break;
+            		}
+            		btemp2.undoMove(opponentMove);
+        		}
+        		if (losingMove != move) {
+        			nothingSpecialMove = move;
+        		}
         	}
         	btemp.undoMove(move);
         }
-        
+
         if (winningMove != null) {
         	return winningMove;
         }
         if ((losingMove != null) && (tieingMove != null)) {
         	return tieingMove;
         }
-        return nothingSpecialMove;
+        if (nothingSpecialMove != null) {
+        	return nothingSpecialMove;
+        }
+        if (tieingMove != null) {
+        	return tieingMove;
+        }
+        return losingMove;
         
     }
     
-    private int heuristics(final Board board, final Color player) {
-    	final int MILIJON = 1000000;
-    	final int POL_MILIJONA = 500000;
-    	final Color winner = board.getWinner(getOpponent(player));  //TODO check it
-    	if (winner != null) {
-			if (winner.equals(player))
-				return MILIJON;
-			if (winner.equals(Color.EMPTY))
-				return -POL_MILIJONA;
-			else
-				return -MILIJON;
-    	}
-		
-		return random.nextInt(100);
-    }
     
 }
