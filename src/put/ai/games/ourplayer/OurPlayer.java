8caa98d5394678864022c090e71ee261569a9ce0
Sebastian Krzyszkowiak <dos@dosowisko.net>
2015-01-31 18:15:37 +0100

    amazing AI by dos

--- src/put/ai/games/ourplayer/OurPlayer.java
+++ src/put/ai/games/ourplayer/OurPlayer.java
@@ -8,8 +8,6 @@ import put.ai.games.game.Player;
 
 public class OurPlayer extends Player {
 
-    private Random random=new Random(0xdeadbeef);
-
     @Override
     public String getName() {
         return "Sebastian Krzyszkowiak 106553 Piotr Siupa 106605";
@@ -17,8 +15,39 @@ public class OurPlayer extends Player {
 
     @Override
     public Move nextMove(Board b) {
+    	
         List<Move> moves = b.getMovesFor(getColor());
-        return moves.get(moves.size()-1-random.nextInt(moves.size()));
+        
+        Board btemp = b.clone();
+        
+        Move winningMove = null, losingMove = null, tieingMove = null, nothingSpecialMove = null;
+        
+        for (Move move: moves) {
+        	btemp.doMove(move);
+        	Color winner = btemp.getWinner(getOpponent(getColor()));
+        	if (winner == getColor()) {
+        		winningMove = move;
+        	}
+        	if (winner == getOpponent(getColor())) {
+        		losingMove = move;
+        	}
+        	if (winner == Color.EMPTY) {
+        		tieingMove = move;
+        	}
+        	if (winner == null) {
+        		nothingSpecialMove = move;
+        	}
+        	btemp.undoMove(move);
+        }
+        
+        if (winningMove != null) {
+        	return winningMove;
+        }
+        if ((losingMove != null) && (tieingMove != null)) {
+        	return tieingMove;
+        }
+        return nothingSpecialMove;
+        
     }
     
 }
